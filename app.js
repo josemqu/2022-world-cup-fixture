85f737b17516134b404f1783b4944f013ecf974b
José <mailjmq@gmail.com>
2022-11-06 22:48:12 -0300

    bootstrap table styling

--- app.js
+++ app.js
@@ -161,196 +161,174 @@ const countries = {
 	},
 };
 
-const teams = [
-	{
-		id: "QA",
-		name: "Qatar",
-		flagClass: "flag-qatar",
-	},
-	{
-		id: "EC",
-		name: "Ecuador",
-		flagClass: "flag-ecuador",
-	},
-	{
-		id: "SN",
-		name: "Senegal",
-		flagClass: "flag-senegal",
-	},
-	{
-		id: "NL",
-		name: "Paises Bajos",
-		flagClass: "flag-netherlands",
-	},
+// const teams = [
+// 	{
+// 		id: "QA",
+// 		name: "Qatar",
+// 		flagClass: "flag-qatar",
+// 	},
+// 	{
+// 		id: "EC",
+// 		name: "Ecuador",
+// 		flagClass: "flag-ecuador",
+// 	},
+// 	{
+// 		id: "SN",
+// 		name: "Senegal",
+// 		flagClass: "flag-senegal",
+// 	},
+// 	{
+// 		id: "NL",
+// 		name: "Paises Bajos",
+// 		flagClass: "flag-netherlands",
+// 	},
 
-	{
-		id: "EN",
-		name: "Inglaterra",
-		flagClass: "flag-england",
-	},
-	{
-		id: "IR",
-		name: "Iran",
-		flagClass: "flag-iran",
-	},
-	{
-		id: "US",
-		name: "USA",
-		flagClass: "flag-us",
-	},
-	{
-		id: "WA",
-		name: "Gales",
-		flagClass: "flag-wales",
-	},
-	{
-		id: "AR",
-		name: "Argentina",
-		flagClass: "flag-argentina",
-	},
-	{
-		id: "SA",
-		name: "Arabia Saudita",
-		flagClass: "flag-saudi-arabia",
-	},
-	{
-		id: "MX",
-		name: "Mexico",
-		flagClass: "flag-mexico",
-	},
-	{
-		id: "PL",
-		name: "Polonia",
-		flagClass: "flag-poland",
-	},
-	{
-		id: "FR",
-		name: "Francia",
-		flagClass: "flag-france",
-	},
-	{
-		id: "AU",
-		name: "Australia",
-		flagClass: "flag-australia",
-	},
-	{
-		id: "DK",
-		name: "Dinamarca",
-		flagClass: "flag-denmark",
-	},
-	{
-		id: "TN",
-		name: "Tunez",
-		flagClass: "flag-tunisia",
-	},
-	{
-		id: "ES",
-		name: "España",
-		flagClass: "flag-spain",
-	},
-	{
-		id: "CR",
-		name: "Costa Rica",
-		flagClass: "flag-costa-rica",
-	},
-	{
-		id: "DE",
-		name: "Alemania",
-		flagClass: "flag-germany",
-	},
-	{
-		id: "JP",
-		name: "Japon",
-		flagClass: "flag-japan",
-	},
-	{
-		id: "BE",
-		name: "Belgica",
-		flagClass: "flag-belgium",
-	},
-	{
-		id: "CA",
-		name: "Canada",
-		flagClass: "flag-canada",
-	},
-	{
-		id: "MA",
-		name: "Marruecos",
-		flagClass: "flag-morocco",
-	},
-	{
-		id: "HR",
-		name: "Croacia",
-		flagClass: "flag-croatia",
-	},
-	{
-		id: "BR",
-		name: "Brasil",
-		flagClass: "flag-brazil",
-	},
-	{
-		id: "RS",
-		name: "Serbia",
-		flagClass: "flag-serbia",
-	},
-	{
-		id: "CH",
-		name: "Suiza",
-		flagClass: "flag-switzerland",
-	},
-	{
-		id: "CM",
-		name: "Camerun",
-		flagClass: "flag-cameroon",
-	},
-	{
-		id: "PT",
-		name: "Portugal",
-		flagClass: "flag-portugal",
-	},
-	{
-		id: "GH",
-		name: "Ghana",
-		flagClass: "flag-ghana",
-	},
-	{
-		id: "UY",
-		name: "Uruguay",
-		flagClass: "flag-uruguay",
-	},
-	{
-		id: "KR",
-		name: "Corea del Sur",
-		flagClass: "flag-south-korea",
-	},
-];
-const groupArr = [
-	{
-		id: "QA",
-		name: "Qatar",
-		flagClass: "flag-qatar",
-	},
-	{
-		id: "EC",
-		name: "Ecuador",
-		flagClass: "flag-ecuador",
-	},
-	{
-		id: "SN",
-		name: "Senegal",
-		flagClass: "flag-senegal",
-	},
-	{
-		id: "NL",
-		name: "Paises Bajos",
-		flagClass: "flag-netherlands",
-	},
-];
+// 	{
+// 		id: "EN",
+// 		name: "Inglaterra",
+// 		flagClass: "flag-england",
+// 	},
+// 	{
+// 		id: "IR",
+// 		name: "Iran",
+// 		flagClass: "flag-iran",
+// 	},
+// 	{
+// 		id: "US",
+// 		name: "USA",
+// 		flagClass: "flag-us",
+// 	},
+// 	{
+// 		id: "WA",
+// 		name: "Gales",
+// 		flagClass: "flag-wales",
+// 	},
+// 	{
+// 		id: "AR",
+// 		name: "Argentina",
+// 		flagClass: "flag-argentina",
+// 	},
+// 	{
+// 		id: "SA",
+// 		name: "Arabia Saudita",
+// 		flagClass: "flag-saudi-arabia",
+// 	},
+// 	{
+// 		id: "MX",
+// 		name: "Mexico",
+// 		flagClass: "flag-mexico",
+// 	},
+// 	{
+// 		id: "PL",
+// 		name: "Polonia",
+// 		flagClass: "flag-poland",
+// 	},
+// 	{
+// 		id: "FR",
+// 		name: "Francia",
+// 		flagClass: "flag-france",
+// 	},
+// 	{
+// 		id: "AU",
+// 		name: "Australia",
+// 		flagClass: "flag-australia",
+// 	},
+// 	{
+// 		id: "DK",
+// 		name: "Dinamarca",
+// 		flagClass: "flag-denmark",
+// 	},
+// 	{
+// 		id: "TN",
+// 		name: "Tunez",
+// 		flagClass: "flag-tunisia",
+// 	},
+// 	{
+// 		id: "ES",
+// 		name: "España",
+// 		flagClass: "flag-spain",
+// 	},
+// 	{
+// 		id: "CR",
+// 		name: "Costa Rica",
+// 		flagClass: "flag-costa-rica",
+// 	},
+// 	{
+// 		id: "DE",
+// 		name: "Alemania",
+// 		flagClass: "flag-germany",
+// 	},
+// 	{
+// 		id: "JP",
+// 		name: "Japon",
+// 		flagClass: "flag-japan",
+// 	},
+// 	{
+// 		id: "BE",
+// 		name: "Belgica",
+// 		flagClass: "flag-belgium",
+// 	},
+// 	{
+// 		id: "CA",
+// 		name: "Canada",
+// 		flagClass: "flag-canada",
+// 	},
+// 	{
+// 		id: "MA",
+// 		name: "Marruecos",
+// 		flagClass: "flag-morocco",
+// 	},
+// 	{
+// 		id: "HR",
+// 		name: "Croacia",
+// 		flagClass: "flag-croatia",
+// 	},
+// 	{
+// 		id: "BR",
+// 		name: "Brasil",
+// 		flagClass: "flag-brazil",
+// 	},
+// 	{
+// 		id: "RS",
+// 		name: "Serbia",
+// 		flagClass: "flag-serbia",
+// 	},
+// 	{
+// 		id: "CH",
+// 		name: "Suiza",
+// 		flagClass: "flag-switzerland",
+// 	},
+// 	{
+// 		id: "CM",
+// 		name: "Camerun",
+// 		flagClass: "flag-cameroon",
+// 	},
+// 	{
+// 		id: "PT",
+// 		name: "Portugal",
+// 		flagClass: "flag-portugal",
+// 	},
+// 	{
+// 		id: "GH",
+// 		name: "Ghana",
+// 		flagClass: "flag-ghana",
+// 	},
+// 	{
+// 		id: "UY",
+// 		name: "Uruguay",
+// 		flagClass: "flag-uruguay",
+// 	},
+// 	{
+// 		id: "KR",
+// 		name: "Corea del Sur",
+// 		flagClass: "flag-south-korea",
+// 	},
+// ];
+
 const groupTemplate = document.querySelector("#group-template").content;
 const teamTemplate = document.querySelector("#team-template").content;
 const groupTables = document.querySelectorAll(".group-table");
 const fragment = document.createDocumentFragment();
-const grupoA = document.querySelector(".grupoA tbody");
 
 const groupDataA = ["QA", "EC", "SN", "NL"];
 const groupDataB = ["EN", "IR", "US", "WA"];
